7b30346abc59a09c1cf5c9fe7a6a7d7aec887f38
Timendum <timedum@gmail.com>
2022-03-21 15:43:12 +0100

    Zip: ignore more file, produce only Firefox

--- zip.sh
+++ zip.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #package for firefox
-zip -r -FS ../HNES-firefox.zip * -x \*.git\* *screenshots\* *notes* *zip.sh* js/jquery-3.2.1.js
+zip -r -FS HNES-firefox.zip * -x \*.git\* *screenshots\* *notes* *zip.sh* js/jquery-3.2.1.js .eslintrc README.md *.zip
 
 #package for chrome web store
-cd ..
-zip -r -FS HNES-chrome.zip HNES -x \*.git\* *screenshots* HNES/notes HNES/zip.sh HNES/js/jquery-3.2.1.js
+# cd ..
+# zip -r -FS HNES-chrome.zip HNES -x \*.git\* *screenshots* HNES/notes HNES/zip.sh HNES/js/jquery-3.2.1.js
